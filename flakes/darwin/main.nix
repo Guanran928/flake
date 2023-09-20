0a06c22469861d20bdd70c4ce896f6fb1915180f
Guanran Wang <guanran928@outlook.com>
2023-09-20 10:35:12 +0800

    darwin: add more keybind to skhd

--- flakes/darwin/main.nix
+++ flakes/darwin/main.nix
@@ -63,6 +63,18 @@
       enable = true;
       skhdConfig = ''
         cmd - return : open -n ${pkgs.alacritty}/Applications/Alacritty.app
+
+        cmd - 1 : yabai -m space --focus 1             # Focus space
+        cmd - 2 : yabai -m space --focus 2
+        cmd - 3 : yabai -m space --focus 3
+        cmd - 4 : yabai -m space --focus 4
+        cmd - 5 : yabai -m space --focus 5
+
+        shift + cmd - 1 : yabai -m window --space 1    # Send to space
+        shift + cmd - 2 : yabai -m window --space 2
+        shift + cmd - 3 : yabai -m window --space 3
+        shift + cmd - 4 : yabai -m window --space 4
+        shift + cmd - 5 : yabai -m window --space 5
       '';
     };
   };
