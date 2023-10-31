c3c7abb669f7bc3fd947027babb5eb94d5f7ba15
Guanran Wang <guanran928@outlook.com>
2023-10-31 17:18:50 +0800

    overlays,sway: use .patch file from github PR

--- nixos/packages/overlays/sway.nix
+++ nixos/packages/overlays/sway.nix
@@ -7,24 +7,11 @@
         {
           sway-unwrapped = prev.sway-unwrapped.overrideAttrs (old: {
             # Add input panel to sway
-            # .patch file from some random guy's dotfile repo
+            # .patch file from https://github.com/swaywm/sway/pull/7226
             patches = (old.patches or []) ++ [
-              # attempt #1, didnt work
-              #(prev.fetchpatch {
-              #  url = "https://aur.archlinux.org/cgit/aur.git/plain/7226.patch?h=sway-im-git";
-              #  hash = "sha256-KPWsxDQ2Zkya6o+llQVRHeulecDPsZAJ2vyQNWZKAps=";
-              #})
-
-              # attempt #2, also didnt work
-              #(prev.fetchpatch {
-              #  url = "https://aur.archlinux.org/cgit/aur.git/plain/0001-text_input-Implement-input-method-popups.patch?h=sway-im";
-              #  hash = "sha256-xrBnQhtA6LgyW0e0wKwymlMvx/JfrjBidq1a3GFKzZo=";
-              #})
-
-              # attempt #3, worked, very buggy
-              (prev.fetchpatch {
-                url = "https://raw.githubusercontent.com/slaier/nixos-config/main/modules/sway/0001-text_input-Implement-input-method-popups.patch";
-                hash = "sha256-f3xI2Pz3rfF2aBfZlC/4wMF/UphKcEVHSCZ1/23AndQ=";
+              (prev.fetchurl {
+                url = "https://github.com/swaywm/sway/commit/d1c6e44886d1047b3aa6ff6aaac383eadd72f36a.patch";
+                sha256 = "sha256-UnNnAgXVBPjhF7ytVpGEStbJK1RQuRIci5PgGEvLp80=";
               })
             ];
           });
