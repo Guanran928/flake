b973f3176aec4689720539858138bf6bbc198aa8
Guanran Wang <guanran928@outlook.com>
2023-11-04 18:22:10 +0800

    darwin: add proxy to nix-daemon

--- users/guanranwang/darwin/presets/core/proxy.nix
+++ users/guanranwang/darwin/presets/core/proxy.nix
@@ -2,25 +2,23 @@
   inputs,
   pkgs,
   ...
-}:
-# README!!:
-# I HAVE NO IDEA HOW LAUNCHD DAEMON WORKS
-# USE AT YOUR OWN RISK
-# it just works™
-#
-# Remember to manually copy `config.yaml` to `/etc/clash-meta`
-# I have no idea how to get sops-nix working on darwin...
-#
-{
-  launchd.daemons."clash-meta" = {
-    command = "${pkgs.clash-meta}/bin/clash-meta -d /etc/clash-meta";
-  };
-
+}: {
   environment.variables = {
     "http_proxy" = "http://127.0.0.1:7890";
     "https_proxy" = "http://127.0.0.1:7890";
     "ftp_proxy" = "http://127.0.0.1:7890";
     "rsync_proxy" = "http://127.0.0.1:7890";
+  };
+
+  launchd.daemons."nix-daemon".environment = {
+    "http_proxy" = "http://127.0.0.1:7890";
+    "https_proxy" = "http://127.0.0.1:7890";
+    "ftp_proxy" = "http://127.0.0.1:7890";
+    "rsync_proxy" = "http://127.0.0.1:7890";
+  };
+
+  launchd.daemons."clash-meta" = {
+    command = "${pkgs.clash-meta}/bin/clash-meta -d /etc/clash-meta";
   };
 
   environment.etc."clash-meta/metacubexd".source = inputs.metacubexd;
