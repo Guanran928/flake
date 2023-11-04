63e6aa1b813ecbfd0671d08c8c74e40d8173019d
Guanran Wang <guanran928@outlook.com>
2023-11-04 15:36:48 +0800

    flake: install git

--- nixos/nix/flake.nix
+++ nixos/nix/flake.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # Enable Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Install Git
+  environment.systemPackages = [ pkgs.git ];
 
   # Does not work with Flake based configurations
   system.copySystemConfiguration = false;
