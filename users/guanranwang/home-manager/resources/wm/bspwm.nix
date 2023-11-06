404eacd81de128bd23e57643a890b0336323d948
Guanran Wang <guanran928@outlook.com>
2023-11-06 16:07:02 +0800

    home,bspwm: bspwmrc -> home-manager configuration

--- users/guanranwang/home-manager/resources/wm/bspwm.nix
+++ users/guanranwang/home-manager/resources/wm/bspwm.nix
@@ -11,7 +11,21 @@
     enable = true;
     windowManager.bspwm = {
       enable = true;
-      extraConfig = builtins.readFile ../dotfiles/config/bspwm/bspwmrc;
+      startupPrograms = [
+        "pgrep -x sxhkd > /dev/null || sxhkd"
+        "pgrep -x feh > /dev/null || feh --bg-fill ~/.local/share/backgrounds/wallpaper1.png"
+      ];
+      monitors = {
+        eDP-1 = ["I" "II" "III" "IV" "V" "VI" "VII" "VIII" "IX" "X"];
+      };
+      settings = {
+        border_width = 2;
+        window_gap = 4;
+        focus_follows_pointer = true;
+        split_ratio = 0.52;
+        borderless_monocle = true;
+        gapless_monocle = true;
+      };
     };
   };
 }
