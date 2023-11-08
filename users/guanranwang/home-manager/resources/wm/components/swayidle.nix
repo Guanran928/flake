55b8095a3217d4994c4c2a94c176bc5126b05d0b
Guanran Wang <guanran928@outlook.com>
2023-11-08 19:59:42 +0800

    home,idle: suspend device after 20min

--- users/guanranwang/home-manager/resources/wm/components/swayidle.nix
+++ users/guanranwang/home-manager/resources/wm/components/swayidle.nix
@@ -10,12 +10,16 @@
         {
           timeout = 900;
           command = "swaylock";
-        }
+        } # lock screen
         {
           timeout = 905;
-          command = ''${pkgs.sway}/bin/swaymsg "output * dpms off"'';
-          resumeCommand = ''${pkgs.sway}/bin/swaymsg "output * dpms on"'';
+          command = "${pkgs.sway}/bin/swaymsg \"output * dpms off\"";
+          resumeCommand = "${pkgs.sway}/bin/swaymsg \"output * dpms on\"";
         } # turn off screen
+        {
+          timeout = 1200;
+          command = ''systemctl suspend'';
+        } # suspend
       ];
       events = [
         {
