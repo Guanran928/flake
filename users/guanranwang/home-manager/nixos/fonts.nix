6e84d833eb3c6856fe60285656db1532b5a5d138
Guanran Wang <guanran928@outlook.com>
2023-11-08 15:34:00 +0800

    home,nixos,fonts: noto cjk -> source han & fontconfig changes

--- users/guanranwang/home-manager/nixos/fonts.nix
+++ users/guanranwang/home-manager/nixos/fonts.nix
@@ -1,30 +1,25 @@
 {pkgs, ...}: {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
-    ### Noto Fonts
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-emoji
-
-    ### Source Han
-    source-han-sans
-    source-han-serif
-    source-han-mono
-
     ### Sans
     inter
-    roboto
 
     ### Monospace
-    fira-code
     jetbrains-mono
     (nerdfonts.override {
       fonts = [
-        "FiraCode"
         "JetBrainsMono"
         "NerdFontsSymbolsOnly"
       ];
     })
+
+    ### Adobe Source Han
+    source-han-sans
+    source-han-serif
+    source-han-mono
+
+    ### Noto Fonts
+    noto-fonts
+    noto-fonts-emoji
   ];
 }
