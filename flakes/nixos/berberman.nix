90f5949e702f879d209cc8f0d8a7a3f043614736
Guanran Wang <guanran928@outlook.com>
2023-11-10 01:27:22 +0800

    nixos,flakes: lib.mkAfter

--- flakes/nixos/berberman.nix
+++ flakes/nixos/berberman.nix
@@ -1,9 +1,13 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   nixpkgs.overlays = [
     inputs.berberman.overlays.default
   ];
   nix.settings = {
-    substituters = ["https://berberman.cachix.org"];
+    substituters = lib.mkAfter ["https://berberman.cachix.org"];
     trusted-public-keys = ["berberman.cachix.org-1:UHGhodNXVruGzWrwJ12B1grPK/6Qnrx2c3TjKueQPds="];
   };
 }
--- flakes/nixos/hyprland.nix
+++ flakes/nixos/hyprland.nix
@@ -1,6 +1,6 @@
-{...}: {
+{lib, ...}: {
   nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
+    substituters = lib.mkAfter ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
 }
