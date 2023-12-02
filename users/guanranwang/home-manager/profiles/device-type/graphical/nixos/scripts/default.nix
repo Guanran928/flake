f38bf906919bd1eb4cab13c76859fb6c821ef7e3
Guanran Wang <guanran928@outlook.com>
2023-12-02 17:22:38 +0800

    home, scripts: use fetchers

--- users/guanranwang/home-manager/profiles/device-type/graphical/nixos/scripts/default.nix
+++ users/guanranwang/home-manager/profiles/device-type/graphical/nixos/scripts/default.nix
@@ -1,8 +1,17 @@
-{...}: {
+{pkgs, ...}: {
   home.sessionPath = ["$HOME/.local/bin"];
-  home.file.".local/bin" = {
-    source = ./bin;
-    recursive = true;
-    executable = true;
+  home.file = {
+    ".local/bin" = {
+      source = ./bin;
+      recursive = true;
+      executable = true;
+    };
+    ".local/bin/lofi" = {
+      source = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/lime-desu/bin/main/lofi";
+        hash = "sha256-hT+S/rqOHUYnnFcSDFfQht4l1DGasz1L3wDHKUWLraA=";
+      };
+      executable = true;
+    };
   };
 }
