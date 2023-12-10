4dba4df4bdfe6634d42a24029dadcd2f183e7b15
Guanran Wang <guanran928@outlook.com>
2023-12-11 00:11:24 +0800

    nixos: nix: flakes: disable flake-registry

--- nixos/profiles/core/nix/flake.nix
+++ nixos/profiles/core/nix/flake.nix
@@ -1,9 +1,19 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   # Enable Flakes
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # Disable nix-channel
   nix.channel.enable = false;
+
+  # Disable flake-registry
+  # https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry
+  nix.settings.flake-registry = "";
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
+  nix.registry.nixpkgs-stable.flake = inputs.nixpkgs-stable;
 
   # Install Git
   environment.systemPackages = [pkgs.git];
