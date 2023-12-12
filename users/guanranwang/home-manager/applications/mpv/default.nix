0bbf04fabe1fb93ca642b904005db23e9edfc189
Guanran Wang <guanran928@outlook.com>
2023-12-13 02:53:45 +0800

    home: configure mpv

--- users/guanranwang/home-manager/applications/mpv/default.nix
+++ users/guanranwang/home-manager/applications/mpv/default.nix
@@ -1,3 +1,29 @@
-_: {
-  programs.mpv.enable = true;
+{pkgs, ...}: {
+  programs.mpv = {
+    enable = true;
+    config = {
+      hwdec = "auto-safe";
+      vo = "gpu-next";
+      profile = "gpu-hq";
+      osc = "no";
+    };
+    scripts = with pkgs.mpvScripts; [
+      mpris
+      thumbfast
+    ];
+  };
+
+  # for scripts that is not in nixpkgs
+  xdg.configFile = let
+    modernX = pkgs.fetchFromGitHub {
+      owner = "zydezu";
+      repo = "modernX";
+      rev = "10f74d29f86cbfb307181ecfa564402f993b073d";
+      hash = "sha256-ozbOsVGdllB+E4UtERynhe5Eng3nKnL/IIEtT4yVdOI=";
+    };
+  in {
+    "mpv/scripts/modernx.lua".source = "${modernX}/modernx.lua";
+    "mpv/fonts/Material-Design-Iconic-Font.ttf".source = "${modernX}/Material-Design-Iconic-Font.ttf";
+    "mpv/fonts/Material-Design-Iconic-Round.ttf".source = "${modernX}/Material-Design-Iconic-Round.ttf";
+  };
 }
