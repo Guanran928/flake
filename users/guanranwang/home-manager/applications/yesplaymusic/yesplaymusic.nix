dab79d237922601b839e759c4ee6a40688044618
Guanran Wang <guanran928@outlook.com>
2023-12-13 02:52:19 +0800

    home: fix yesplaymusic

--- users/guanranwang/home-manager/applications/yesplaymusic/yesplaymusic.nix
+++ users/guanranwang/home-manager/applications/yesplaymusic/yesplaymusic.nix
@@ -1,21 +1,3 @@
 {pkgs, ...}: {
-  imports = [./spicetify.nix];
-  programs.mpv.enable = true;
-  home.packages = with pkgs; [
-    # Music & Video
-    ### Local
-    amberol
-
-    ### Streaming
-    yesplaymusic
-    netease-cloud-music-gtk
-
-    ### Misc
-    mousai
-    cava
-    easyeffects
-
-    # Photos
-    loupe
-  ];
+  home.packages = [pkgs.yesplaymusic];
 }
