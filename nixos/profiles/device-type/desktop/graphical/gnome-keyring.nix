207007fc9c8a27c81111da531afb57582ce3bb19
Guanran Wang <guanran928@outlook.com>
2023-12-14 02:06:39 +0800

    nixos: gnome-keyring: remove extra stuff

--- nixos/profiles/device-type/desktop/graphical/gnome-keyring.nix
+++ nixos/profiles/device-type/desktop/graphical/gnome-keyring.nix
@@ -1,10 +1,5 @@
-{pkgs, ...}: {
+_: {
   # gnome keyring
   services.gnome.gnome-keyring.enable = true;
   programs.seahorse.enable = true;
-  security.pam.services."gnome-keyring".text = ''
-    auth     optional ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
-    session  optional ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
-    password optional ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
-  '';
 }
