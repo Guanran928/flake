6515e7710868da86f3066f6e26a6c57b7e36a742
Guanran Wang <guanran928@outlook.com>
2023-12-15 23:13:47 +0800

    darwin: move system defaults to home-manager

--- darwin/profiles/device-type/desktop/default.nix
+++ darwin/profiles/device-type/desktop/default.nix
@@ -1,6 +1,5 @@
 {...}: {
   imports = [
     ./packages
-    ./system
   ];
 }
--- users/guanranwang/darwin/profiles/core/home/default.nix
+++ users/guanranwang/darwin/profiles/core/home/default.nix
@@ -2,4 +2,33 @@
   imports = [
     ../../../../home-manager
   ];
+
+  # https://github.com/mathiasbynens/dotfiles/blob/main/.macos
+  targets.darwin.defaults = {
+    ### Inputs
+    ".GlobalPreferences"."com.apple.mouse.scaling" = "-1"; # Disable mouse acceleration
+
+    ### .DS_Store
+    "com.apple.desktopservices" = {
+      DSDontWriteNetworkStores = true;
+      DSDontWriteUSBStores = true;
+    };
+
+    ### Dock
+    "com.apple.dock".autohide = true;
+
+    ### Finder
+    "com.apple.finder" = {
+      _FXShowPosixPathInTitle = false;
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      CreateDesktop = false;
+      FXDefaultSearchScope = "SCcf"; # Current folder
+      FXEnableExtensionChangeWarning = false;
+      FXPreferredViewStyle = "Nlsv"; # List view
+      QuitMenuItem = true;
+      ShowPathbar = true;
+      ShowStatusBar = false;
+    };
+  };
 }
