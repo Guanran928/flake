605554ccce51e25f2cb1af6ecbeb81c83737ca58
Guanran Wang <guanran928@outlook.com>
2023-12-16 18:18:58 +0800

    home: nautilus: show hidden files

--- users/guanranwang/home-manager/applications/nautilus/default.nix
+++ users/guanranwang/home-manager/applications/nautilus/default.nix
@@ -10,5 +10,6 @@
   dconf.settings = {
     "org/gnome/nautilus/list-view".default-zoom-level = "small";
     "org/gnome/nautilus/preferences".default-folder-viewer = "list-view";
+    "org/gtk/gtk4/settings/file-chooser".show-hidden = true;
   };
 }
