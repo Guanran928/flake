635c1d97a6ffb600296e5d3e939efa89625902d0
Guanran Wang <guanran928@outlook.com>
2023-12-17 18:19:27 +0800

    secrets: slight adjustment

--- users/guanranwang/nixos/profiles/core/default.nix
+++ users/guanranwang/nixos/profiles/core/default.nix
@@ -25,7 +25,7 @@
 
   ### sops-nix
   sops = {
-    defaultSopsFile = ../../../secrets/secrets.yaml;
+    defaultSopsFile = ../../../secrets.yaml;
     age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
     gnupg.sshKeyPaths = [];
     secrets = {
