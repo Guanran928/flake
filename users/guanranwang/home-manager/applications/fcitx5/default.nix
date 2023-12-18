c0178f439324b09970346d8495bb358b8e2aa339
Guanran Wang <guanran928@outlook.com>
2023-12-19 03:33:52 +0800

    home: fcitx5: add themes

--- users/guanranwang/home-manager/applications/fcitx5/default.nix
+++ users/guanranwang/home-manager/applications/fcitx5/default.nix
@@ -16,6 +16,20 @@
       ]);
   };
 
+  # Fcitx5 themes
+  xdg.configFile."fcitx5/conf/classicui.conf".text = "Theme=Tokyonight-Storm";
+  xdg.dataFile = let
+    tokyonight = pkgs.fetchFromGitHub {
+      owner = "ch3n9w";
+      repo = "fcitx5-Tokyonight";
+      rev = "f7454ab387d6b071ee12ff7ee819f0c7030fdf2c";
+      hash = "sha256-swOy0kDZUdqtC2sPSZEBLnHSs8dpQ/QfFMObI6BARfo=";
+    };
+  in {
+    "fcitx5/themes/Tokyonight-Day".source = "${tokyonight}/Tokyonight-Day";
+    "fcitx5/themes/Tokyonight-Storm".source = "${tokyonight}/Tokyonight-Storm";
+  };
+
   home.sessionVariables = {
     "FCITX_NO_PREEDIT_APPS" = "gvim.*,wps.*,wpp.*,et.*,cinny,epiphany";
   };
