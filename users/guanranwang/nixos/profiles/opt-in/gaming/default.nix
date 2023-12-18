629e79de880786908988e77bd8bf6ea2e5045374
Guanran Wang <guanran928@outlook.com>
2023-12-19 02:57:04 +0800

    nixos: fix steam not launching

--- users/guanranwang/nixos/profiles/opt-in/gaming/default.nix
+++ users/guanranwang/nixos/profiles/opt-in/gaming/default.nix
@@ -1,4 +1,8 @@
 _: {
   ### home-manager
   home-manager.users.guanranwang.imports = [./home];
+
+  ### for steam
+  # https://github.com/NixOS/nixpkgs/issues/47932
+  hardware.opengl.driSupport32Bit = true;
 }
