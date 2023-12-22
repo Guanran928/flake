cebc08cb78156a4b6065f3855a4c89fd61d8adb8
Guanran Wang <guanran928@outlook.com>
2023-12-22 10:57:03 +0800

    nixos: zram-generator: use optimized sysctl

--- nixos/profiles/opt-in/zram-generator.nix
+++ nixos/profiles/opt-in/zram-generator.nix
@@ -6,4 +6,12 @@ _: {
       zram-size = "ram";
     };
   };
+
+  # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+  };
 }
