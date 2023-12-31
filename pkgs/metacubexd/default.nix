41f9f57d85e62da4db8a21f41135b1e02cb150cc
Guanran Wang <guanran928@outlook.com>
2023-12-31 20:23:03 +0800

    metacubexd: refactor

--- pkgs/metacubexd/default.nix
+++ pkgs/metacubexd/default.nix
@@ -1,12 +1,13 @@
 {
-  fetchurl,
+  lib,
   stdenvNoCC,
+  fetchurl,
 }:
-stdenvNoCC.mkDerivation (finalAttrs: {
+stdenvNoCC.mkDerivation rec {
   pname = "metacubexd";
   version = "1.134.0";
   src = fetchurl {
-    url = "https://github.com/MetaCubeX/metacubexd/releases/download/v${finalAttrs.version}/compressed-dist.tgz";
+    url = "https://github.com/MetaCubeX/metacubexd/releases/download/v${version}/compressed-dist.tgz";
     hash = "sha256-Xx2UReUAxHg4CrKqGs9vGmWRsosJE1OqnYSmp2wOC9M=";
   };
   sourceRoot = ".";
@@ -14,4 +15,10 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     mkdir -p $out
     cp -r ./* $out
   '';
-})
+
+  meta = with lib; {
+    description = "Clash.Meta Dashboard, The Official One, XD";
+    homepage = "https://github.com/MetaCubeX/metacubexd";
+    license = licenses.mit;
+  };
+}
