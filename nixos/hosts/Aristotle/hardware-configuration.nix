d690d256e1364b9f34e772aba2d78b5096bc694f
Guanran Wang <guanran928@outlook.com>
2024-01-03 06:41:42 +0800

    Revert "nixos: hosts: aristotle: add dpi"
    This reverts commit dd8fa2150ff4d4897e9cad7c0f4333ede4e8d225.

--- nixos/hosts/Aristotle/hardware-configuration.nix
+++ nixos/hosts/Aristotle/hardware-configuration.nix
@@ -15,9 +15,6 @@
     tpm.enable = true;
   };
 
-  # √(1920² + 1080²) px ÷ 15.60 in ≃ 141.211998082 dpi
-  services.xserver.dpi = 141;
-
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid"];
   boot.kernelModules = ["kvm-intel"];
   nixpkgs.hostPlatform = "x86_64-linux";
