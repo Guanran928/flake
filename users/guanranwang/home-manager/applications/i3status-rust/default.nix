6acc04ff690a1ecb707cc5ce5a413b13d9815b28
Guanran Wang <guanran928@outlook.com>
2024-01-05 18:32:58 +0800

    home: add i3status-rust

--- users/guanranwang/home-manager/applications/i3status-rust/default.nix
+++ users/guanranwang/home-manager/applications/i3status-rust/default.nix
@@ -1,3 +1,25 @@
 _: {
-  programs.i3status-rust.enable = true;
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      default = {
+        icons = "material-nf";
+        blocks = [
+          ### Monitor
+          {
+            block = "disk_space";
+            path = "/nix";
+          }
+          {block = "memory";}
+          {block = "cpu";}
+          {block = "load";}
+
+          ### Stuff that I actually need
+          {block = "sound";}
+          #{block = "battery";} # i cant get it working
+          {block = "time";}
+        ];
+      };
+    };
+  };
 }
