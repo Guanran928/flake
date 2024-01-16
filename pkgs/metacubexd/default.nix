350947e1d4233d55d9a3427fa4dfb07d92ec4bb5
Guanran Wang <guanran928@outlook.com>
2024-01-16 10:29:31 +0800

    metacubexd: cleanup

--- pkgs/metacubexd/default.nix
+++ pkgs/metacubexd/default.nix
@@ -6,10 +6,15 @@
 stdenvNoCC.mkDerivation rec {
   pname = "metacubexd";
   version = "1.134.0";
+
   src = fetchurl {
     url = "https://github.com/MetaCubeX/metacubexd/releases/download/v${version}/compressed-dist.tgz";
     hash = "sha256-Xx2UReUAxHg4CrKqGs9vGmWRsosJE1OqnYSmp2wOC9M=";
   };
+
+  dontConfigure = true;
+  dontBuild = true;
+
   sourceRoot = ".";
   installPhase = ''
     mkdir -p $out
@@ -20,5 +25,6 @@ stdenvNoCC.mkDerivation rec {
     description = "Clash.Meta Dashboard, The Official One, XD";
     homepage = "https://github.com/MetaCubeX/metacubexd";
     license = licenses.mit;
+    platforms = platforms.all;
   };
 }
