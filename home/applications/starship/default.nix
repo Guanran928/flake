eaf4c735355b9e071cc69ce8ff69a77a4f5eee2a
Guanran Wang <guanran928@outlook.com>
2024-01-18 16:47:24 +0800

    home: starship: use tokyonight preset

--- home/applications/starship/default.nix
+++ home/applications/starship/default.nix
@@ -3,18 +3,22 @@
     enable = true;
     settings =
       {
-        add_newline = false;
-        line_break.disabled = true;
+        #add_newline = false;
+        #line_break.disabled = true;
 
-        character = {
-          success_symbol = "[>](bold green)";
-          error_symbol = "[>](bold red)";
-          vimcmd_symbol = "[<](bold green)";
-          vimcmd_replace_one_symbol = "[<](bold purple)";
-          vimcmd_replace_symbol = "[<](bold purple)";
-          vimcmd_visual_symbol = "[<](bold yellow)";
-        };
+        #character = {
+        #  success_symbol = "[>](bold green)";
+        #  error_symbol = "[>](bold red)";
+        #  vimcmd_symbol = "[<](bold green)";
+        #  vimcmd_replace_one_symbol = "[<](bold purple)";
+        #  vimcmd_replace_symbol = "[<](bold purple)";
+        #  vimcmd_visual_symbol = "[<](bold yellow)";
+        #};
       }
-      // (builtins.fromTOML (builtins.readFile "${pkgs.starship}/share/starship/presets/nerd-font-symbols.toml"));
+      // builtins.fromTOML (builtins.readFile "${pkgs.starship}/share/starship/presets/nerd-font-symbols.toml")
+      // builtins.fromTOML (builtins.readFile (pkgs.substitute {
+        src = "${pkgs.starship}/share/starship/presets/tokyo-night.toml";
+        replacements = ["--replace" "" ""];
+      }));
   };
 }
