eb74f5cdef2436d6c300d3541c11da3cafea9581
Guanran Wang <guanran928@outlook.com>
2024-01-21 00:51:17 +0800

    home: configure tealdeer

--- home/applications/tealdeer/default.nix
+++ home/applications/tealdeer/default.nix
@@ -1,3 +1,9 @@
 {
-  programs.tealdeer.enable = true;
+  programs.tealdeer = {
+    enable = true;
+    settings = {
+      display.use_pager = true;
+      updates.auto_update = true;
+    };
+  };
 }
