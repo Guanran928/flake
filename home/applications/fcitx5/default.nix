4a33b357d250eab0f55c198deba6145f2cf0467b
Guanran Wang <guanran928@outlook.com>
2024-01-28 12:37:57 +0800

    nixos: fcitx5: use fcitx5-tokyonight

--- home/applications/fcitx5/default.nix
+++ home/applications/fcitx5/default.nix
@@ -13,23 +13,13 @@
       ++ (with inputs.berberman.packages.${pkgs.stdenv.hostPlatform.system}; [
         fcitx5-pinyin-moegirl
         fcitx5-pinyin-zhwiki
+      ])
+      ++ (with inputs.self.packages.${pkgs.stdenv.hostPlatform.system}; [
+        fcitx5-tokyonight
       ]);
   };
 
-  # Fcitx5 themes
   xdg.configFile."fcitx5/conf/classicui.conf".text = "Theme=Tokyonight-Storm";
-  xdg.dataFile = let
-    tokyonight = pkgs.fetchFromGitHub {
-      owner = "ch3n9w";
-      repo = "fcitx5-Tokyonight";
-      rev = "f7454ab387d6b071ee12ff7ee819f0c7030fdf2c";
-      hash = "sha256-swOy0kDZUdqtC2sPSZEBLnHSs8dpQ/QfFMObI6BARfo=";
-    };
-  in {
-    "fcitx5/themes/Tokyonight-Day".source = "${tokyonight}/Tokyonight-Day";
-    "fcitx5/themes/Tokyonight-Storm".source = "${tokyonight}/Tokyonight-Storm";
-  };
-
   home.sessionVariables = {
     "FCITX_NO_PREEDIT_APPS" = "gvim.*,wps.*,wpp.*,et.*,cinny,epiphany";
   };
