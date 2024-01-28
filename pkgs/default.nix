79df0364c37add2d2862d08c8f6f72b68453913a
Guanran Wang <guanran928@outlook.com>
2024-01-28 12:09:19 +0800

    fcitx5-tokyonight: init at unstable-2024-01-28
    im a fucking moron
    https://github.com/ch3n9w/fcitx5-Tokyonight/issues/1

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
 pkgs: {
+  fcitx5-tokyonight = pkgs.callPackage ./fcitx5-tokyonight {};
   metacubexd = pkgs.callPackage ./metacubexd {};
   mpvScripts.modernx = pkgs.callPackage ./mpvScripts/modernx {};
 }
