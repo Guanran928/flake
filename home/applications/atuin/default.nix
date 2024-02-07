9ef67cc4be785c32788869943097597373d61626
Guanran Wang <guanran928@outlook.com>
2024-02-07 10:10:16 +0800

    home/atuin: configure stats

--- home/applications/atuin/default.nix
+++ home/applications/atuin/default.nix
@@ -2,6 +2,20 @@
   programs.atuin = {
     enable = true;
     flags = ["--disable-up-arrow"];
-    settings.style = "compact";
+    settings = {
+      style = "compact";
+
+      stats = {
+        common_subcommands =
+          ["nix" "nom" "nh" "podman" "docker" "atuin" "hugo" "adb"]
+          # default
+          ++ ["cargo" "go" "git" "npm" "yarn" "pnpm" "kubectl"];
+
+        common_prefix =
+          ["env"]
+          # default
+          ++ ["sudo"];
+      };
+    };
   };
 }
