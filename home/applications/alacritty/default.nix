59cb9f8a6424667c4f2f25ba6cd0d83980709fbd
Guanran Wang <guanran928@outlook.com>
2024-02-08 22:15:43 +0800

    fixup! home/alacritty: enable option_as_alt

--- home/applications/alacritty/default.nix
+++ home/applications/alacritty/default.nix
@@ -1,9 +1,13 @@
 {
+  lib,
+  pkgs,
+  ...
+}: {
   programs.alacritty = {
     enable = true;
     settings = {
       env.WINIT_X11_SCALE_FACTOR = "1"; # workaround for scaling in X11
-      window.option_as_alt = "Both"; # for zellij on macOS
+      window.option_as_alt = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin "Both"; # for zellij on macOS
     };
   };
 }
