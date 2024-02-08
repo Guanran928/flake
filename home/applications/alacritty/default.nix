1e8876ee811b7745ff1c0ec3fe8bc8f9c2908875
Guanran Wang <guanran928@outlook.com>
2024-02-08 12:36:03 +0800

    home/alacritty: enable option_as_alt

--- home/applications/alacritty/default.nix
+++ home/applications/alacritty/default.nix
@@ -1,6 +1,9 @@
 {
   programs.alacritty = {
     enable = true;
-    settings.env.WINIT_X11_SCALE_FACTOR = "1"; # workaround for scaling in X11
+    settings = {
+      env.WINIT_X11_SCALE_FACTOR = "1"; # workaround for scaling in X11
+      window.option_as_alt = "Both"; # for zellij on macOS
+    };
   };
 }
