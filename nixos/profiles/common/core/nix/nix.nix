29a35e0e9c213416bdad4d8f64e88c9bf0855e93
Guanran Wang <guanran928@outlook.com>
2024-02-11 22:11:31 +0800

    fixup! flake: cleanup

--- nixos/profiles/common/core/nix/nix.nix
+++ nixos/profiles/common/core/nix/nix.nix
@@ -14,14 +14,12 @@
         "https://nix-community.cachix.org"
         "https://cache.garnix.io"
         "https://berberman.cachix.org"
-        "https://hyprland.cachix.org"
         "https://nix-gaming.cachix.org"
       ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "berberman.cachix.org-1:UHGhodNXVruGzWrwJ12B1grPK/6Qnrx2c3TjKueQPds="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
 
