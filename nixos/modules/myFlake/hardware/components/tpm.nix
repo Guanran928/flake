cd91bb36793044515ac3449bf4780a579936fb4a
Guanran Wang <guanran928@outlook.com>
2024-02-12 14:03:25 +0800

    flake: update lock file

--- nixos/modules/myFlake/hardware/components/tpm.nix
+++ nixos/modules/myFlake/hardware/components/tpm.nix
@@ -14,9 +14,11 @@ in {
     # TPM is currently broken on latest kernel,
     # but luckily, linux-zen have a patch for it
     # UPDATE: it got fixed in 6.5.3
+    #
+    # python3.11-tpm2-pytss-2.1.0 failed with exit code 1 after ⏱ 34s
     security.tpm2 = {
       enable = true;
-      pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
+      #pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
       tctiEnvironment.enable = true; # tpm2tools_tcti and tpm2_pkcs11_tcti env variables
     };
   };
