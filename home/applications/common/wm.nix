53b4f1c94b7619931ea92fe5f7d2693fde3757c9
Guanran Wang <guanran928@outlook.com>
2024-02-13 12:24:25 +0800

    home/wm: add GTK_CSD=0

--- home/applications/common/wm.nix
+++ home/applications/common/wm.nix
@@ -8,4 +8,9 @@
       "button-layout" = "icon,appmenu:"; # remove csd window buttons
     };
   };
+
+  home.sessionVariables = {
+    # https://github.com/localsend/localsend/blob/2457acd8a7412723b174672d174e4853dccd7d99/app/linux/my_application.cc#L45
+    GTK_CSD = 0;
+  };
 }
