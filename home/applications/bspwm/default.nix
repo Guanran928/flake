9f9fe8cb8f8bafcd32fcd47be4368e13a78b4bf4
Guanran Wang <guanran928@outlook.com>
2024-02-15 14:21:59 +0800

    repo: add editorconfig && format

--- home/applications/bspwm/default.nix
+++ home/applications/bspwm/default.nix
@@ -61,10 +61,10 @@
 
     # start some nice programs
     if [ -d /etc/X11/xinit/xinitrc.d ] ; then
-     for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
-      [ -x "$f" ] && . "$f"
-     done
-     unset f
+      for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+        [ -x "$f" ] && . "$f"
+      done
+      unset f
     fi
 
     # https://wiki.archlinux.org/title/Cursor_themes#Change_X_shaped_default_cursor
