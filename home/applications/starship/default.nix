448f375e8deb25fc9413ee5f71eb5de034de2e3a
Guanran Wang <guanran928@outlook.com>
2024-02-18 16:50:21 +0800

    home/starship: use default settings
    stay fresh. staring at the same prompt every single day makes me tired.

--- home/applications/starship/default.nix
+++ home/applications/starship/default.nix
@@ -3,17 +3,17 @@
     enable = true;
     settings =
       {
-        add_newline = false;
-        line_break.disabled = true;
+        #add_newline = false;
+        #line_break.disabled = true;
 
-        character = {
-          success_symbol = "[>](bold green)";
-          error_symbol = "[>](bold red)";
-          vimcmd_symbol = "[<](bold green)";
-          vimcmd_replace_one_symbol = "[<](bold purple)";
-          vimcmd_replace_symbol = "[<](bold purple)";
-          vimcmd_visual_symbol = "[<](bold yellow)";
-        };
+        #character = {
+        #  success_symbol = "[>](bold green)";
+        #  error_symbol = "[>](bold red)";
+        #  vimcmd_symbol = "[<](bold green)";
+        #  vimcmd_replace_one_symbol = "[<](bold purple)";
+        #  vimcmd_replace_symbol = "[<](bold purple)";
+        #  vimcmd_visual_symbol = "[<](bold yellow)";
+        #};
       }
       // (builtins.fromTOML (builtins.readFile "${pkgs.starship}/share/starship/presets/nerd-font-symbols.toml"));
   };
