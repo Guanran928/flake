87635c85d19eebc10b09a863272a7707a4050791
Guanran Wang <guanran928@outlook.com>
2024-02-18 18:51:57 +0800

    home/theme: use adwaita icon && cleanup

--- home/applications/nautilus/default.nix
+++ home/applications/nautilus/default.nix
@@ -1,12 +1,5 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   home.packages = [pkgs.gnome.nautilus];
-  xdg.configFile."gtk-3.0/bookmarks".text = ''
-    file://${config.home.homeDirectory}/Documents/Projects/git-repos/github.com/Guanran928/flake
-  '';
   dconf.settings = {
     "org/gnome/nautilus/list-view".default-zoom-level = "small";
     "org/gnome/nautilus/preferences".default-folder-viewer = "list-view";
--- nixos/profiles/common/graphical/home/theme.nix
+++ nixos/profiles/common/graphical/home/theme.nix
@@ -4,49 +4,40 @@
   lib,
   ...
 }: {
-  gtk.enable = true;
-  gtk.gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-
-  ### Cursor
   home.pointerCursor = {
-    gtk.enable = true;
-    x11.enable = true;
     name = "Adwaita";
     package = pkgs.gnome.adwaita-icon-theme;
     size = 24;
+    x11.enable = true;
+    gtk.enable = true;
   };
-  # gtk.cursorTheme is unneeded
-  # https://github.com/nix-community/home-manager/blob/8765d4e38aa0be53cdeee26f7386173e6c65618d/modules/config/home-cursor.nix#L179C33-L179C33
 
-  ### Icon
   gtk = {
+    enable = true;
+    gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+
+    gtk3.bookmarks = [
+      "file://${config.home.homeDirectory}/Documents/Projects/git-repos/github.com/Guanran928/flake"
+    ];
+
     iconTheme = {
       #name = "Tela-dracula-dark";
       #package = pkgs.tela-icon-theme;
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
+      #name = "Papirus-Dark";
+      #package = pkgs.papirus-icon-theme;
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
+    };
+
+    theme = {
+      name = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
     };
   };
 
-  ### GTK
-  gtk.theme = {
-    name = "adw-gtk3-dark";
-    package = pkgs.adw-gtk3;
-  };
+  dconf.settings."org/gnome/desktop/interface"."color-scheme" = "prefer-dark";
 
   # ??? this commit broke nautilus's spacing ???
   # https://github.com/nix-community/home-manager/commit/e9b9ecef4295a835ab073814f100498716b05a96
   xdg.configFile."gtk-4.0/gtk.css".text = lib.mkForce config.gtk.gtk4.extraCss;
-
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      "clock-format" = "12h";
-      "color-scheme" = "prefer-dark";
-    };
-  };
-
-  # X11
-  xresources.extraConfig =
-    lib.mkIf (config.myFlake.home-manager.colorScheme == "tokyonight")
-    (builtins.readFile "${pkgs.vimPlugins.tokyonight-nvim}/extras/xresources/tokyonight_night.Xresources");
 }
