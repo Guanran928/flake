690c0249952a7b46f5a2406d541c2c00c6388da6
Guanran Wang <guanran928@outlook.com>
2024-03-01 04:11:29 +0800

    home/steam: disable proton-ge
    - I don't play Windows games at all and I have no idea why did I add it in the first place...

--- home/applications/steam/default.nix
+++ home/applications/steam/default.nix
@@ -1,11 +1,10 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: {
+{pkgs, ...}: {
   home.packages = [
-    (pkgs.steam.override {
-      extraProfile = "export STEAM_EXTRA_COMPAT_TOOLS_PATHS='${inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.proton-ge}'";
-    })
+    pkgs.steam
+
+    # with ProtonGE
+    # (pkgs.steam.override {
+    #   extraProfile = "export STEAM_EXTRA_COMPAT_TOOLS_PATHS='${inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.proton-ge}'";
+    # })
   ];
 }
