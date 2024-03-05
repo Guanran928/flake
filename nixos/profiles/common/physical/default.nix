a963d7fc39385464ba5e98c920e437401f19a185
Guanran Wang <guanran928@outlook.com>
2024-03-06 05:08:53 +0800

    nixos/physical: add yubikey

--- nixos/profiles/common/physical/default.nix
+++ nixos/profiles/common/physical/default.nix
@@ -1,6 +1,9 @@
-{
+{pkgs, ...}: {
   networking.stevenblack.enable = true;
   services.system76-scheduler.enable = true;
   services.power-profiles-daemon.enable = true;
   services.thermald.enable = true;
+
+  # YubiKey
+  services.udev.packages = [pkgs.yubikey-personalization];
 }
