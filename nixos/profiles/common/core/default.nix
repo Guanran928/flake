5b4d2db89122c4d366aec777055f4b9df7211a27
Guanran Wang <guanran928@outlook.com>
2024-04-01 12:27:58 +0800

    fixup! nixos: add xz backdoor killswitch

--- nixos/profiles/common/core/default.nix
+++ nixos/profiles/common/core/default.nix
@@ -119,4 +119,5 @@
   # xz backdoor killswitch
   # ref: https://gist.github.com/thesamesam/223949d5a074ebc3dce9ee78baad9e27?permalink_comment_id=5006861#gistcomment-5006861
   environment.sessionVariables."yolAbejyiejuvnup" = "Evjtgvsh5okmkAvj";
+  systemd.services."sshd".environment."yolAbejyiejuvnup" = "Evjtgvsh5okmkAvj";
 }
