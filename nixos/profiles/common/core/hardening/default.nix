c9af571fd49928441ca72009d585a6f9781f325c
Guanran Wang <guanran928@outlook.com>
2024-05-01 08:19:25 +0800

    nixos/hardening: remove systemd service hardening

--- nixos/profiles/common/core/hardening/default.nix
+++ nixos/profiles/common/core/hardening/default.nix
@@ -4,7 +4,6 @@
   # ref: https://madaidans-insecurities.github.io/guides/linux-hardening.html
   imports = [
     ./sysctl.nix
-    ./systemd.nix
   ];
 
   environment.etc.machine-id.text = "b08dfa6083e7567a1921a715000001fb"; # whonix id
