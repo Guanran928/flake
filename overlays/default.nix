5725422b8ff6e104e1afff347fc6706787175784
Guanran Wang <guanran928@outlook.com>
2024-05-03 08:10:55 +0800

    nixos: unbreak fcitx5

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,5 +9,6 @@ in {
     // import ./nautilus.nix {inherit addPatches prev;}
     // import ./prismlauncher.nix {inherit addPatches prev;}
     // import ./sway.nix {inherit addPatches prev;}
-    // import ./tailscale.nix {inherit addPatches prev;};
+    // import ./tailscale.nix {inherit addPatches prev;}
+    // import ./fcitx5.nix {inherit addPatches prev;};
 }
