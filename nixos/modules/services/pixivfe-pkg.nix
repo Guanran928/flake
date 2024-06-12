2141256716b65aa7314410d9dcae475963aaf36c
Guanran Wang <guanran928@outlook.com>
2024-06-12 17:01:49 +0800

    nixos: update pixivfe

--- nixos/modules/services/pixivfe-pkg.nix
+++ nixos/modules/services/pixivfe-pkg.nix
@@ -6,17 +6,19 @@
 }:
 buildGoModule rec {
   pname = "pixivfe";
-  version = "2.5.1";
+  version = "2.6";
 
   src = fetchFromGitea {
     domain = "codeberg.org";
     owner = "VnPower";
     repo = "PixivFE";
     rev = "v${version}";
-    hash = "sha256-G2pSPpemMFAbQ9QkI4XAHobv+Em9ZoDUJiO/cwEy4Tc=";
+    hash = "sha256-pusyCXy2tsdvOSUR6LfSYHv8YT1tiCErqUEkUgKYbZ4=";
   };
 
   vendorHash = "sha256-QapDR964Tn+RxXdkGqCQXacdmlSapF841Y84n4d/6VI=";
+
+  ldflags = ["-s" "-w"];
 
   nativeBuildInputs = [makeBinaryWrapper];
 
