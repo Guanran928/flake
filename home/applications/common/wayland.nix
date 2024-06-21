e9200310c9623ab070b30c620e18a70e193e4dd3
Guanran Wang <guanran928@outlook.com>
2024-06-21 11:03:42 +0800

    fixup! treewide: remove unused files

--- home/applications/common/wayland.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  home.sessionVariables = {
-    NIXOS_OZONE_WL = "1"; # let electron applications use wayland
-  };
-
-  home.packages = with pkgs; [
-    wl-clipboard
-  ];
-}
--- home/applications/common/wm.nix
+++
@@ -1,8 +0,0 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [pwvucontrol];
-
-  # remove csd window buttons
-  # https://github.com/localsend/localsend/blob/2457acd8a7412723b174672d174e4853dccd7d99/app/linux/my_application.cc#L45
-  home.sessionVariables.GTK_CSD = 0;
-  dconf.settings."org/gnome/desktop/wm/preferences"."button-layout" = "icon,appmenu:";
-}
