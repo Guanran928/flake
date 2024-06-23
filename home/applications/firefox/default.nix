045bf55f6c5083b2a26daa74afd6cf7e0c2da525
Guanran Wang <guanran928@outlook.com>
2024-06-23 23:20:57 +0800

    flake: update lock file

--- home/applications/firefox/default.nix
+++ home/applications/firefox/default.nix
@@ -1,14 +1,16 @@
 {pkgs, ...}: {
   programs.firefox = {
     enable = true;
-    profiles."default" = {
-      extraConfig = ''
-        ${builtins.readFile (builtins.fetchurl {
-          url = "https://raw.githubusercontent.com/arkenfox/user.js/126.1/user.js";
-          sha256 = "sha256-XRtG0iLKh8uqbeX7Rc2H6VJwZYJoNZPBlAfZEfrSCP4=";
-        })}
-        ${builtins.readFile ./user-overrides.js}
-      '';
+    package = pkgs.firefox.override {
+      extraPrefsFiles = [
+        "${pkgs.arkenfox-userjs}/user.cfg"
+        (pkgs.runCommandLocal "userjs" {} ''
+          install -Dm644 ${./user-overrides.js} $out
+          substituteInPlace $out \
+            --replace-fail "user_pref" "defaultPref"
+        '')
+      ];
     };
+    profiles."default" = {};
   };
 }
