4055e7432a26f0074f69b62e0b29c71ce4931485
Guanran Wang <guanran928@outlook.com>
2024-06-23 22:55:42 +0800

    home/{firefox,thunderbird}: remove ifd

--- home/applications/firefox/default.nix
+++ home/applications/firefox/default.nix
@@ -3,17 +3,12 @@
     enable = true;
     profiles."default" = {
       extraConfig = ''
-        ${builtins.readFile (pkgs.fetchurl {
-          # FIXME: IFD
+        ${builtins.readFile (builtins.fetchurl {
           url = "https://raw.githubusercontent.com/arkenfox/user.js/126.1/user.js";
-          hash = "sha256-XRtG0iLKh8uqbeX7Rc2H6VJwZYJoNZPBlAfZEfrSCP4=";
+          sha256 = "sha256-XRtG0iLKh8uqbeX7Rc2H6VJwZYJoNZPBlAfZEfrSCP4=";
         })}
         ${builtins.readFile ./user-overrides.js}
       '';
     };
-  };
-
-  home.sessionVariables = {
-    MOZ_USE_XINPUT2 = "1";
   };
 }
--- home/applications/thunderbird/default.nix
+++ home/applications/thunderbird/default.nix
@@ -1,13 +1,12 @@
-{pkgs, ...}: {
+{
   programs.thunderbird = {
     enable = true;
     profiles.default = {
       isDefault = true;
       extraConfig = ''
-        ${builtins.readFile (pkgs.fetchurl {
-          # FIXME: IFD
+        ${builtins.readFile (builtins.fetchurl {
           url = "https://raw.githubusercontent.com/HorlogeSkynet/thunderbird-user.js/d6b18302e46349d9924c8a76951bae6efca51501/user.js";
-          hash = "sha256-66B1yLQkQnydAUXD7KGt32OhWSYcdWX+BUozrgW9uAg=";
+          sha256 = "sha256-66B1yLQkQnydAUXD7KGt32OhWSYcdWX+BUozrgW9uAg=";
         })}
         ${builtins.readFile ./user-overrides.js}
       '';
