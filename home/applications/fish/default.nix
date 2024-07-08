d86e047588f2cccc311138a0a44be7d26c0cc31a
Guanran Wang <guanran928@outlook.com>
2024-07-08 17:55:52 +0800

    home/fish: cleanup plugins
    those seems to be not working for some unknown reasons

--- home/applications/fish/default.nix
+++ home/applications/fish/default.nix
@@ -13,35 +13,9 @@
         name = "done";
         inherit (pkgs.fishPlugins.done) src;
       }
-      #{
-      #  name = "tide";
-      #  src = pkgs.fishPlugins.tide.src;
-      #}
-      {
-        name = "sponge";
-        inherit (pkgs.fishPlugins.sponge) src;
-      }
       {
         name = "puffer";
         inherit (pkgs.fishPlugins.puffer) src;
-      }
-      {
-        name = "sudope";
-        src = pkgs.fetchFromGitHub {
-          owner = "oh-my-fish";
-          repo = "plugin-sudope";
-          rev = "83919a692bc1194aa322f3627c859fecace5f496";
-          hash = "sha256-pD4rNuqg6TG22L9m8425CO2iqcYm8JaAEXIVa0H/v/U=";
-        };
-      }
-      {
-        name = "fish-abbreviation-tips";
-        src = pkgs.fetchFromGitHub {
-          owner = "gazorby";
-          repo = "fish-abbreviation-tips";
-          rev = "8ed76a62bb044ba4ad8e3e6832640178880df485";
-          hash = "sha256-F1t81VliD+v6WEWqj1c1ehFBXzqLyumx5vV46s/FZRU=";
-        };
       }
     ];
   };
