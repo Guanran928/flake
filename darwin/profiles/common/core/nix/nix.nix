b23abd8a85ec0f0240c71dad314863cac045dfa8
Guanran Wang <guanran928@outlook.com>
2024-07-10 05:45:51 +0800

    nix: cleanup substituters

--- darwin/profiles/common/core/nix/nix.nix
+++ darwin/profiles/common/core/nix/nix.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   pkgs,
   config,
   ...
@@ -6,23 +7,15 @@
   nix.settings = {
     trusted-users = ["@admin"];
     substituters =
-      {
-        "Asia/Shanghai" = [
-          "https://mirror.sjtu.edu.cn/nix-channels/store" # SJTU - 上海交通大学 Mirror
-          "https://mirrors.ustc.edu.cn/nix-channels/store" # USTC - 中国科学技术大学 Mirror
-          "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" # TUNA - 清华大学 Mirror
-        ];
-      }
-      .${config.time.timeZone}
-      or []
+      (lib.optionals (config.time.timeZone == "Asia/Shanghai") [
+        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" # TUNA - 清华大学 Mirror
+      ])
       ++ [
         "https://nix-community.cachix.org"
-        "https://cache.garnix.io"
         "https://guanran928.cachix.org"
       ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "guanran928.cachix.org-1:BE/iBCj2/pqJXG908wHRrcaV0B2fC+KbFjHsXY6b91c="
     ];
     use-xdg-base-directories = true;
