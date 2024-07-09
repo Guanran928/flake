a20a63696f483e670bde007d49ca8087743b6b22
Guanran Wang <guanran928@outlook.com>
2024-07-09 15:14:46 +0800

    home/gaming: enable sdl3 for osu-lazer

--- nixos/profiles/common/opt-in/gaming/home/default.nix
+++ nixos/profiles/common/opt-in/gaming/home/default.nix
@@ -3,14 +3,13 @@
 
   home.packages = with pkgs; [
     (prismlauncher.override {glfw = glfw-wayland-minecraft;})
-    (steam.override {
-      extraEnv = {
-        # STEAM_EXTRA_COMPAT_TOOLS_PATHS = gamePkgs.proton-ge;
-      };
-    })
+    steam
     mumble
     osu-lazer-bin
-    # lunar-client
-    # protonup-qt
   ];
+
+  home.sessionVariables = {
+    # https://github.com/ppy/osu-framework/pull/6292
+    "OSU_SDL3" = "1";
+  };
 }
