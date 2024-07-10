a779cb029f09f228e8410a8a2becef1c0fbeace6
Guanran Wang <guanran928@outlook.com>
2024-07-10 16:33:00 +0800

    nixos/wireless: fix ssid

--- nixos/profiles/opt-in/wireless/default.nix
+++ nixos/profiles/opt-in/wireless/default.nix
@@ -1,7 +1,7 @@
 {lib, ...}: {
   sops.secrets = builtins.mapAttrs (_name: value: value // {sopsFile = ./secrets.yaml;}) {
     "wireless/wangxiaobo".path = "/var/lib/iwd/wangxiaobo.psk";
-    "wireless/OpenWrt".path = "/var/lib/iwd/OpenWrt.psk";
+    "wireless/ImmortalWrt".path = "/var/lib/iwd/ImmortalWrt.psk";
   };
 
   networking.wireless.iwd.enable = lib.mkDefault true;
