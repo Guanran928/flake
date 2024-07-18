14b75192ccccb9a2f92e6ebb123ebf4225bac2b3
Guanran Wang <guanran928@outlook.com>
2024-07-18 17:47:30 +0800

    tyo0/miniflux: use keycloak oidc

--- hosts/lightsail-tokyo/default.nix
+++ hosts/lightsail-tokyo/default.nix
@@ -42,7 +42,7 @@
       "searx/environment" = {
         restartUnits = ["searx.service"];
       };
-      "miniflux/credentials" = {
+      "miniflux/environment" = {
         restartUnits = ["miniflux.service"];
       };
     };
@@ -285,10 +285,16 @@
 
   services.miniflux = {
     enable = true;
-    adminCredentialsFile = config.sops.secrets."miniflux/credentials".path;
+    adminCredentialsFile = config.sops.secrets."miniflux/environment".path;
     config = {
       LISTEN_ADDR = "127.0.0.1:9300";
       BASE_URL = "https://rss.ny4.dev";
+
+      OAUTH2_PROVIDER = "oidc";
+      OAUTH2_CLIENT_ID = "miniflux";
+      # OAUTH2_CLIENT_SECRET = "replace_me"; # EnvironmentFile
+      OAUTH2_REDIRECT_URL = "https://rss.ny4.dev/oauth2/oidc/callback";
+      OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://id.ny4.dev/realms/ny4";
     };
   };
 
