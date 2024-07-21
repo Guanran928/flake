2ea64a046c6ab07f1dd168c5a283f808fca10399
Guanran Wang <guanran928@outlook.com>
2024-07-21 16:18:47 +0800

    home/i3status-rust: cleanup

--- home/applications/i3status-rust/default.nix
+++ home/applications/i3status-rust/default.nix
@@ -1,26 +1,14 @@
 {
   programs.i3status-rust = {
     enable = true;
-    bars = {
-      default = {
-        icons = "material-nf";
-        blocks = [
-          ### Monitor
-          {
-            block = "disk_space";
-            path = "/nix";
-          }
-          {block = "memory";}
-          {block = "cpu";}
-          {block = "load";}
-
-          ### Stuff that I actually need
-          {block = "backlight";}
-          {block = "sound";}
-          {block = "battery";}
-          {block = "time";}
-        ];
-      };
+    bars.default = {
+      icons = "material-nf";
+      blocks = [
+        {block = "backlight";}
+        {block = "sound";}
+        {block = "battery";}
+        {block = "time";}
+      ];
     };
   };
 }
