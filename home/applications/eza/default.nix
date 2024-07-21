cebe5df3d0d84b6dbedf0c609496333ca7638c7a
Guanran Wang <guanran928@outlook.com>
2024-07-21 17:30:53 +0800

    home/eza: cleanup

--- home/applications/eza/default.nix
+++ home/applications/eza/default.nix
@@ -1,15 +1,10 @@
 {
   programs.eza = {
     enable = true;
-    git = true;
     icons = true;
-    extraOptions = ["--header"];
-    # Fish have builtin aliases for `ls`, alias `ls` to `eza` is the only thing we want to do
-    #enableAliases = true;
   };
 
   home.shellAliases = {
-    "ls" = "eza";
-    "tree" = "ls --tree";
+    "tree" = "eza --tree";
   };
 }
