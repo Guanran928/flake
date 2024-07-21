30512e5772d7ea40f521ea11d028fe7a0e369c2f
Guanran Wang <guanran928@outlook.com>
2024-07-21 18:12:10 +0800

    treewide: replace fetchpatch with fetchpatch2

--- overlays/default.nix
+++ overlays/default.nix
@@ -24,33 +24,33 @@ in {
     sway-unwrapped = addPatches prev.sway-unwrapped [
       # text_input: Implement input-method popups
       # https://github.com/swaywm/sway/pull/7226
-      (prev.fetchpatch rec {
+      (prev.fetchpatch2 rec {
         name = "0001-text_input-Implement-input-method-popups.patch";
         url = "https://aur.archlinux.org/cgit/aur.git/plain/${name}?h=sway-im&id=b8434b3ad9e8c6946dbf7b14b0f7ef5679452b94";
-        hash = "sha256-A+rBaWMWs616WllVoo21AJaf9lxg/oCG0b9tHLfuJII=";
+        hash = "sha256-aO21HgHVccD8vOlffcenSAn2spW7iEs0nTa5Tmebe3o=";
       })
-      (prev.fetchpatch rec {
+      (prev.fetchpatch2 rec {
         name = "0002-chore-fractal-scale-handle.patch";
         url = "https://aur.archlinux.org/cgit/aur.git/plain/${name}?h=sway-im&id=b8434b3ad9e8c6946dbf7b14b0f7ef5679452b94";
-        hash = "sha256-YOFm0A4uuRSuiwnvF9xbp8Wl7oGicFGnq61vLegqJ0E=";
+        hash = "sha256-QuV8J0sqh5L9kyYEOTDjWlPNKVb6zolG/cHO+wq2Qa8=";
       })
-      (prev.fetchpatch rec {
+      (prev.fetchpatch2 rec {
         name = "0003-chore-left_pt-on-method-popup.patch";
         url = "https://aur.archlinux.org/cgit/aur.git/plain/${name}?h=sway-im&id=b8434b3ad9e8c6946dbf7b14b0f7ef5679452b94";
-        hash = "sha256-PzhQBRpyB1WhErn05UBtBfaDW5bxnQLRKWu8jy7dEiM=";
+        hash = "sha256-4zvVbAdxK05UWy+QMsHPHrVBwmO5279GqhYgJUPsCNI=";
       })
 
       # Tray D-Bus Menu
       # https://github.com/swaywm/sway/pull/6249
-      (prev.fetchpatch {
+      (prev.fetchpatch2 {
         name = "0001-Tray-Implement-dbusmenu.patch";
         url = "https://github.com/NickHu/sway/commit/0fc5d7aed84415a77b718ca9dc3c0b3ad2c05b02.patch";
-        hash = "sha256-1KuGZGwyGJK8KO4OngS+tWKV/3Yu++bCNnp+xTrlGoY=";
+        hash = "sha256-eosg6m2P6e8NRXjOAQL3SZg+Yc4Av9Wd+LOy0G3+xkA=";
       })
-      (prev.fetchpatch {
+      (prev.fetchpatch2 {
         name = "0002-Tray-dont-invoke-dbus-menu-when-tray-is-disabled.patch";
         url = "https://github.com/NickHu/sway/commit/03c14421354e54332e12f78d029dcaa9919fd161.patch";
-        hash = "sha256-GhBlCnk7aB6s57wV1FNOPAt6s0oJxLgf2bMw+8ktn8A=";
+        hash = "sha256-8RGtpfN/tnkA7nuGdXGHoKUoKVeG7brSQR6V4RU3z88=";
       })
     ];
   };
