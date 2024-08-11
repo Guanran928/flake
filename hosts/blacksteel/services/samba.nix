312645fe2fc700ac6c3bf191461bab5b819bfdb6
Guanran Wang <guanran928@outlook.com>
2024-08-11 21:26:53 +0800

    blacksteel/samba: use external only

--- hosts/blacksteel/services/samba.nix
+++ hosts/blacksteel/services/samba.nix
@@ -2,15 +2,9 @@
   services.samba = {
     enable = true;
     openFirewall = true;
-    shares = {
-      "share" = {
-        path = "/srv/samba/share";
-        "read only" = "no";
-      };
-      "external" = {
-        path = "/mnt";
-        "read only" = "no";
-      };
+    shares."external" = {
+      "path" = "/mnt";
+      "read only" = "no";
     };
   };
 
@@ -18,8 +12,4 @@
     enable = true;
     openFirewall = true;
   };
-
-  systemd.tmpfiles.rules = [
-    "d /srv/samba/share 0755 guanranwang root"
-  ];
 }
