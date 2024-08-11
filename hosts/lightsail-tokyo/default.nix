538fffeee5d4249a3cc2fa2f91bdec2e4f109113
Guanran Wang <guanran928@outlook.com>
2024-08-11 19:02:04 +0800

    tyo0: use postgresql_16

--- hosts/lightsail-tokyo/default.nix
+++ hosts/lightsail-tokyo/default.nix
@@ -94,6 +94,25 @@
     };
   };
 
+  services.postgresql = {
+    package = pkgs.postgresql_16;
+    settings = {
+      max_connections = 200;
+      shared_buffers = "256MB";
+      effective_cache_size = "768MB";
+      maintenance_work_mem = "64MB";
+      checkpoint_completion_target = 0.9;
+      wal_buffers = "7864kB";
+      default_statistics_target = 100;
+      random_page_cost = 1.1;
+      effective_io_concurrency = 200;
+      work_mem = "655kB";
+      huge_pages = "off";
+      min_wal_size = "1GB";
+      max_wal_size = "4GB";
+    };
+  };
+
   services.wastebin = {
     enable = true;
     settings.WASTEBIN_ADDRESS_PORT = "127.0.0.1:8200";
