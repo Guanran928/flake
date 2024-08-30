31c243e73a3bd86b533215a905136240e72e5fdb
Guanran Wang <guanran928@outlook.com>
2024-08-31 04:23:40 +0800

    home/firefox: update userjs

--- home/applications/firefox/user-overrides.js
+++ home/applications/firefox/user-overrides.js
@@ -6,13 +6,10 @@ user_pref("browser.newtabpage.enabled", true);
 user_pref("browser.shell.shortcutFavicons", true);
 user_pref("browser.startup.homepage", "about:home");
 user_pref("browser.startup.page", 1);
-user_pref("privacy.resistFingerprinting", false);
-user_pref("privacy.resistFingerprinting.letterboxing", false);
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);
 user_pref("security.OCSP.enabled", 0);
 user_pref("security.pki.crlite_mode", 2);
 user_pref("security.remote_settings.crlite_filters.enabled", true);
-user_pref("webgl.disabled", false);
 
 // Weird stuff that is not disabled
 user_pref("browser.preferences.moreFromMozilla", false);
@@ -25,6 +22,7 @@ user_pref("signon.rememberSignons", false);
 // Neat features, nice to have
 user_pref("browser.compactmode.show", true);
 user_pref("browser.search.separatePrivateDefault", false);
+user_pref("browser.tabs.inTitlebar", 0);
 user_pref("browser.urlbar.suggest.calculator", true);
 
 // Smooth scrolling
