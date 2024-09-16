6f0cd4da417d5561775c36d1ccfa3ab081d63b8c
Guanran Wang <guanran928@outlook.com>
2024-09-16 13:07:58 +0800

    home/fish: use vim keybinds

--- home/applications/fish/default.nix
+++ home/applications/fish/default.nix
@@ -5,6 +5,18 @@
 
     interactiveShellInit = ''
       set fish_greeting
+
+      set fish_cursor_default     block
+      set fish_cursor_insert      line
+      set fish_cursor_replace_one underscore
+      set fish_cursor_replace     underscore
+      set fish_cursor_external    line
+      set fish_cursor_visual      block
+
+      function fish_user_key_bindings
+        fish_default_key_bindings -M insert
+        fish_vi_key_bindings --no-erase insert
+      end
     '';
 
     plugins = [
