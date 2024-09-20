4b8df9fa2bfb2aff3b8017242be6acc47bb7938d
Guanran Wang <guanran928@outlook.com>
2024-09-21 01:42:07 +0800

    nixos/wireless: add school wifi & mobile hotspot

--- nixos/profiles/wireless/default.nix
+++ nixos/profiles/wireless/default.nix
@@ -1,9 +1,21 @@
 { lib, ... }:
 {
-  sops.secrets = lib.mapAttrs (_name: value: value // { sopsFile = ./secrets.yaml; }) {
-    "wireless/wangxiaobo".path = "/var/lib/iwd/wangxiaobo.psk";
-    "wireless/ImmortalWrt".path = "/var/lib/iwd/ImmortalWrt.psk";
-  };
+  sops.secrets = lib.listToAttrs (
+    lib.map
+      (
+        x:
+        lib.nameValuePair "wireless/${x}" {
+          path = "/var/lib/iwd/${x}.psk";
+          sopsFile = ./secrets.yaml;
+        }
+      )
+      [
+        "Galaxy S24 EC54"
+        "ImmortalWrt"
+        "XYC-SEEWO"
+        "wangxiaobo"
+      ]
+  );
 
   networking.wireless.iwd.enable = true;
 }
