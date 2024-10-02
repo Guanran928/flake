8dabc942104f33972c542d165074ffc99e2768d1
Guanran Wang <guanran928@outlook.com>
2024-10-02 19:47:24 +0800

    home/firefox: declarative extensions

--- home/applications/firefox/default.nix
+++ home/applications/firefox/default.nix
@@ -12,6 +12,30 @@
         '')
       ];
     };
+
+    policies.ExtensionSettings = {
+      "redirector@einaregilsson.com" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/redirector/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "sponsorBlocker@ajay.app" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "uBlock0@raymondhill.net" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+        installation_mode = "force_installed";
+      };
+    };
+
     profiles."default" = { };
   };
 }
