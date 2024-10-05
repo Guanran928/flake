31d6a1ec801d11157297e16aa42a149c610d95c5
Guanran Wang <guanran928@outlook.com>
2024-10-05 20:13:21 +0800

    sin0/redlib: add settings

--- hosts/vultr/sin0/services/redlib.nix
+++ hosts/vultr/sin0/services/redlib.nix
@@ -9,19 +9,18 @@ in
     address = "127.0.0.1";
   };
 
+  systemd.services."redlib".environment = {
+    # Google's indexing caused a DoS with 800k requests...
+    # https://developers.google.com/search/docs/crawling-indexing/block-indexing
+    REDLIB_ROBOTS_DISABLE_INDEXING = "on";
+    REDLIB_DEFAULT_USE_HLS = "on";
+  };
+
   services.caddy.settings.apps.http.servers.srv0.routes = lib.singleton {
     match = lib.singleton { host = [ "reddit.ny4.dev" ]; };
-    handle = [
-      {
-        # Google's indexing caused a DoS with 800k requests...
-        # https://developers.google.com/search/docs/crawling-indexing/block-indexing
-        handler = "headers";
-        response.set."X-Robots-Tag" = [ "noindex" ];
-      }
-      {
-        handler = "reverse_proxy";
-        upstreams = [ { dial = "localhost:${toString port}"; } ];
-      }
-    ];
+    handle = lib.singleton {
+      handler = "reverse_proxy";
+      upstreams = [ { dial = "localhost:${toString port}"; } ];
+    };
   };
 }
