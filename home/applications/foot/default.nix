22c5bbfc13d139d2088bdc9bead274403756b608
Guanran Wang <guanran928@outlook.com>
2024-11-15 21:21:08 +0800

    home/foot: set url.label-letters

--- home/applications/foot/default.nix
+++ home/applications/foot/default.nix
@@ -5,6 +5,7 @@
       main.font = "monospace:size=10";
       main.resize-by-cells = false;
       cursor.style = "beam";
+      url.label-letters = "aoeuhtns";
     } // import ./tokyonight_night.nix;
   };
 }
