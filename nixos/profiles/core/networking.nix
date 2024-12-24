983f8c8a3d7285d2696a5fb41ce339ba520f81f6
Guanran Wang <guanran928@outlook.com>
2024-12-24 13:58:22 +0800

    nixos/networking: remove dns workaround

--- nixos/profiles/core/networking.nix
+++ nixos/profiles/core/networking.nix
@@ -1,12 +1,6 @@
 {
   services.resolved.enable = true;
 
-  # FIXME: for some reason nslookup is not working
-  networking.nameservers = [
-    "223.5.5.5"
-    "223.6.6.6"
-  ];
-
   ### https://wiki.archlinux.org/title/Sysctl#Improving_performance
   boot.kernelModules = [ "tcp_bbr" ];
   boot.kernel.sysctl = {
