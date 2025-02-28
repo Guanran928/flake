b5ddf918e80f428a6beec62114cb1abaf188d8e2
Guanran Wang <guanran928@outlook.com>
2025-02-28 18:27:57 +0800

    home/foot: fix cursor setting not applying

--- home/applications/foot/default.nix
+++ home/applications/foot/default.nix
@@ -7,13 +7,13 @@
 {
   programs.foot = {
     enable = true;
-    settings = {
+    settings = lib.recursiveUpdate {
       main.font = "monospace:size=10";
       main.resize-by-cells = false;
       main.shell = "${lib.getExe pkgs.tmux} new-session -t main";
       cursor.style = "beam";
       url.label-letters = "aoeuhtns";
-    } // import ./tokyonight_night.nix;
+    } (import ./tokyonight_night.nix);
   };
 
   home.sessionVariables."TERMINAL" = config.programs.foot.package;
