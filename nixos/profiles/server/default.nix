2d5a91ffa13965bef2802903f96d63256f013af4
Guanran Wang <guanran928@outlook.com>
2025-05-04 14:06:56 +0800

    nixos/server: add fido2 ssh keys

--- nixos/profiles/server/default.nix
+++ nixos/profiles/server/default.nix
@@ -8,6 +8,8 @@
 
   users.users."root".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMmd/uqiBahzKcKMJ+gT3dkUIdrWQgudspsDchDlx1E/ guanran928@outlook.com"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIGK/s8rGfSdz7nxot6IDP90eS7OAkmDGUtcQdttd76mRAAAABHNzaDo= guanran928@outlook.com"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIIbx1Dv5rBb1TJ62adWoxgo5eSeDooAw+puweQtx/zAnAAAABHNzaDo= guanran928@outlook.com"
   ];
 
   time.timeZone = "UTC";
