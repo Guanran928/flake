37855a557f8973b564bdecebc3555b99c8fcf121
Guanran Wang <guanran928@outlook.com>
2025-09-10 16:09:00 +0800

    tyo0/postgresql: use postgresql_17

--- hosts/tyo0/default.nix
+++ hosts/tyo0/default.nix
@@ -237,21 +237,29 @@
   ];
 
   services.postgresql = {
-    package = pkgs.postgresql_16;
+    enable = true;
+    package = pkgs.postgresql_17;
     settings = {
       max_connections = 200;
-      shared_buffers = "256MB";
-      effective_cache_size = "768MB";
-      maintenance_work_mem = "64MB";
+      shared_buffers = "512MB";
+      effective_cache_size = "1536MB";
+      maintenance_work_mem = "128MB";
       checkpoint_completion_target = 0.9;
-      wal_buffers = "7864kB";
+      wal_buffers = "16MB";
       default_statistics_target = 100;
       random_page_cost = 1.1;
       effective_io_concurrency = 200;
-      work_mem = "655kB";
+      work_mem = "2520kB";
       huge_pages = "off";
       min_wal_size = "1GB";
       max_wal_size = "4GB";
     };
+  };
+
+  services.postgresqlBackup = {
+    enable = true;
+    location = "/var/lib/backup/postgresql";
+    compression = "zstd";
+    startAt = "weekly";
   };
 }
