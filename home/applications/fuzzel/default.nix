818e038e3d6e6fcbd144acb3402cea08c40f0876
Guanran Wang <guanran928@outlook.com>
2025-10-09 02:38:58 +0800

    home/fuzzel: use dark mode

--- home/applications/fuzzel/default.nix
+++ home/applications/fuzzel/default.nix
@@ -2,9 +2,26 @@
   programs.fuzzel = {
     enable = true;
     settings = {
-      main.horizontal-pad = 10;
-      main.image-size-ratio = 0;
-      border.radius = 0;
+      main = {
+        horizontal-pad = 10;
+        image-size-ratio = 0;
+      };
+
+      border = {
+        radius = 0;
+      };
+
+      # FIXME: Temporary color scheme
+      # https://github.com/kuripa/oxocarbon-fuzzel/blob/master/output/oxocarbon-dark.ini
+      colors = {
+        background = "161616ff";
+        text = "ffffffff";
+        match = "ee5396ff";
+        selection-match = "ee5396ff";
+        selection = "262626ff";
+        selection-text = "33b1ffff";
+        border = "525252ff";
+      };
     };
   };
 }
