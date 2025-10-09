8d5123274cdea469642b7dcc2a4c42e5b97010a8
Guanran Wang <guanran928@outlook.com>
2025-10-09 19:30:52 +0800

    home/swayidle: dim screen before suspend

--- home/applications/swayidle/default.nix
+++ home/applications/swayidle/default.nix
@@ -1,8 +1,33 @@
-{ lib, osConfig, ... }:
+{
+  lib,
+  pkgs,
+  osConfig,
+  ...
+}:
+let
+  brightnessctl = lib.getExe pkgs.brightnessctl;
+in
 {
   services.swayidle = {
     enable = true;
     timeouts = [
+      {
+        timeout = (60 * 5) - 30;
+        command = toString (
+          pkgs.writeScript "dim-screen"
+            # bash
+            ''
+              current=$(${brightnessctl} get)
+              max=$(${brightnessctl} max)
+              low=$(($max / 10))
+
+              if [ "$current" -gt "$low" ]; then
+                  ${brightnessctl} --save set $low
+              fi
+            ''
+        );
+        resumeCommand = "${brightnessctl} --restore";
+      }
       {
         timeout = 60 * 5;
         command = "/run/current-system/sw/bin/systemctl suspend";
