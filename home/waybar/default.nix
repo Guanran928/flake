91e443f83ad06f25023368dfb74c59ac0327db7e
Guanran Wang <guanran928@outlook.com>
2025-11-22 07:42:14 +0800

    home/waybar: use module

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -1,11 +1,6 @@
+{ pkgs, ... }:
 {
-  pkgs,
-  lib,
-  config,
-  ...
-}:
-{
-  home.packages = [ pkgs.waybar ];
+  programs.waybar.enable = true;
 
   xdg.configFile."waybar/config.jsonc" = {
     source = ./config.jsonc;
@@ -21,32 +16,8 @@
     '';
   };
 
-  systemd.user.services.waybar = {
-    Unit = {
-      Description = "Highly customizable Wayland bar for Sway and Wlroots based compositors.";
-      Documentation = "https://github.com/Alexays/Waybar/wiki";
-      PartOf = [
-        config.wayland.systemd.target
-        "tray.target"
-      ];
-      After = [ config.wayland.systemd.target ];
-      ConditionEnvironment = "WAYLAND_DISPLAY";
-      X-Restart-Triggers = [
-        (toString config.xdg.configFile."waybar/config.jsonc".source)
-        (toString config.xdg.configFile."waybar/style.css".source)
-      ];
-    };
-
-    Service = {
-      ExecReload = "${pkgs.coreutils}/bin/kill -SIGUSR2 $MAINPID";
-      ExecStart = lib.getExe pkgs.waybar;
-      KillMode = "mixed";
-      Restart = "on-failure";
-    };
-
-    Install.WantedBy = [
-      config.wayland.systemd.target
-      "tray.target"
-    ];
-  };
+  systemd.user.services.waybar.Unit.X-Reload-Triggers = [
+    ./config.jsonc
+    ./style.css
+  ];
 }
