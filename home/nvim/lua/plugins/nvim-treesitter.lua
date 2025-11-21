bb2b114f3cd0afb11afe8279aa282c38626f7047
Guanran Wang <guanran928@outlook.com>
2025-11-22 07:34:07 +0800

    home/nvim: fix treesitter

--- home/nvim/lua/plugins/nvim-treesitter.lua
+++ home/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,17 +1,19 @@
 ---@type LazyPluginSpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "main",
+  branch = "master", -- `main` is too restricted
   build = ":TSUpdate",
   event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
   lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
   opts = {
     ensure_installed = {
+      -- keep-sorted start
       "bash",
       "c",
       "comment",
       "diff",
       "html",
+      "javascript",
       "json",
       "jsonc",
       "lua",
@@ -19,15 +21,24 @@ return {
       "markdown",
       "markdown_inline",
       "nix",
+      "rust",
       "toml",
+      "tsx",
+      "typescript",
       "vim",
       "vimdoc",
       "xml",
       "yaml",
+      -- keep-sorted end
     },
 
+    auto_install = true,
     highlight = { enable = true },
     indent = { enable = true },
     folds = { enable = true },
   },
+  config = function(_, opts)
+    require("nvim-treesitter.install").prefer_git = true
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
