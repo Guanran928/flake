cb355b41efeb0a451e61c344fdd1d8f8570e5691
Guanran Wang <guanran928@outlook.com>
2026-01-07 00:03:27 +0800

    infra/pocketid: import user

--- infra/pocketid.tf
+++ infra/pocketid.tf
@@ -4,7 +4,10 @@ resource "pocketid_client" "immich" {
   callback_urls = [
     "https://immich.ny4.dev/auth/login",
     "https://immich.ny4.dev/user-settings",
-    "app.immich:///oauth-callback",
+    # FIXME: broken by URL validation
+    # The value "app.immich:///oauth-callback" is not a valid URL: must include scheme and host
+    #
+    # "app.immich:///oauth-callback",
   ]
   allowed_user_groups = [
     "a5dfd8ba-9b7b-4c6a-92d8-d60ce52fb19b",
@@ -42,5 +45,14 @@ resource "pocketid_client" "synapse" {
     "https://matrix.ny4.dev/_synapse/client/oidc/callback",
   ]
   pkce_enabled = false
+}
+
+resource "pocketid_user" "nyancat" {
+  username   = "nyancat"
+  email      = "guanran928@outlook.com"
+  groups     = ["a5dfd8ba-9b7b-4c6a-92d8-d60ce52fb19b"]
+  is_admin   = true
+  first_name = "Guanran"
+  last_name  = "Wang"
 }
 # keep-sorted end
